03878dd9ee9ac32e6f0cb738d6583d6e88f81e62
Barashkov <ivanbarashkov@yandex.ru>
2024-01-19 16:41:55 +0300

    v1.5

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import telebot;
+
+import telebot
 from telebot import types
 import requests
 import config
@@ -40,24 +41,36 @@ condition = {
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    keyboard = types.ReplyKeyboardMarkup()
-    button_geo = types.KeyboardButton(text="Автоматически", request_location=True)
-    button_choise = types.KeyboardButton(text="Вручную")
-    keyboard.add(button_geo)
-    keyboard.add(button_choise)
+    # keyboard = types.ReplyKeyboardMarkup()
+    # button_geo = types.KeyboardButton(text="Автоматически", request_location=True)
+    # button_choise = types.KeyboardButton(text="Вручную")
+    # keyboard.add(button_geo)
+    # keyboard.add(button_choise)
+    keyboard = types.InlineKeyboardMarkup()
+    button1 = types.InlineKeyboardButton("Автоматически", callback_data='automatically')
+    button2 = types.InlineKeyboardButton("Вручную", callback_data='manually')
+    keyboard.add(button1, button2)
     bot.send_message(message.chat.id, f"Для передачи погоды, выбери способ определения местоложения",
                      reply_markup=keyboard)
 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    try:
+        if call.message:
+            if call.data == "automatically":
+                bot.send_location(call.message.chat.id,57.566729, 39.935673)
+                bot.send_message(call.message.chat.id, "Пожалуйста, отправьте ваше местоположение", reply_markup=types.sendlocation())
+            if call.data == "manually":
+                bot.send_message(call.message.chat.id, "Ничего, все наладится!")
+    except Exception as e:
+        print(repr(e))
 
 @bot.message_handler(content_types=["location"])
 def location(message):
     if message.location is not None:
         bot.send_message(message.chat.id,
                          yandex_weather(message.location.latitude, message.location.longitude))
-        geolocator = Nominatim(user_agent="my_geocoder")
-        mylocation = geolocator.reverse((message.location.latitude, message.location.longitude), language="ru")
-        bot.send_message(message.chat.id, mylocation.address)
-        start(message)
+    start(message)
 
 
 @bot.message_handler(content_types=['text'])
@@ -68,6 +81,10 @@ def func(message):
         bot.send_message(message.chat.id, text="Выберите ближайшее местоположение", reply_markup=markup)
     elif message.text in address:
         bot.send_message(message.chat.id, yandex_weather(57.566729, 39.935673))
+        user = message.from_user
+        name = user.first_name
+        username = user.username
+        bot.send_message(message.chat.id, f'{name}, {username}')
         start(message)
     else:
         bot.send_message(message.chat.id, text="На такую комманду я не запрограммирован..")
@@ -91,7 +108,10 @@ def yandex_weather(latitude, longitude):
         # Выводим код ошибки
         print(f'Ошибка: {yandex_req.status_code}')
 
-    google_write(data)
+    geolocator = Nominatim(user_agent="my_geocoder")
+    mylocation = geolocator.reverse((latitude, longitude), language="ru")
+
+    google_write(data, mylocation.address.split(',')[1] + ' ' + mylocation.address.split(',')[0])
     return f'Температура воздуха: {data["fact"]["temp"]} °C' + '\n' + \
         f'Ощущается как: {data["fact"]["feels_like"]} °C' + '\n' + \
         f'Скорость ветра: {data["fact"]["wind_speed"]} м/с' + '\n' + \
@@ -101,14 +121,14 @@ def yandex_weather(latitude, longitude):
         f'Описание погоды: {condition[data["fact"]["condition"]]}'
 
 
-def google_write(data) -> None:
+def google_write(data, adress) -> None:
     # Указываем путь к JSON
     gc = gspread.service_account(filename='credentials.json')
     # Открываем таблицу для записи
     sh = gc.open("air_quality_measurement")
     nextcol = len(sh.sheet1.col_values(2)) + 1
     sh.sheet1.update(values=[[int(sh.sheet1.cell(nextcol - 1, 1).value) + 1, datetime.now().strftime('%H:%M:%S'),
-                              datetime.now().strftime('%d.%m.%Y'), 'адрес', data["fact"]["temp"],
+                              datetime.now().strftime('%d.%m.%Y'), adress, data["fact"]["temp"],
                               data["fact"]["wind_speed"],
                               wind_dir[data["fact"]["wind_dir"]], data["fact"]["pressure_mm"], data["fact"]["humidity"],
                               condition[data["fact"]["condition"]]]], range_name=f'A{nextcol}:J{nextcol}')
